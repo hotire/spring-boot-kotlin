29e8d722e8f8e1f1a451626b3b48452aa64d4239
hotire <gngh0101@gmail.com>
2021-01-08 09:24:03 +0900

    Add test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,11 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 tasks.withType<Test> {
