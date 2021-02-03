d5cdb451af58626b27c7cc3461630e97f9da8bbc
hotire <gngh0101@gmail.com>
2021-02-04 00:56:23 +0900

    Add kotlin-core

---
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/core/CoroutinesController.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springbootkotlin.books.core;
+
+/**
+ * https://github.com/sdeleuze/spring-boot-coroutines-demo/blob/master/src/main/kotlin/com/example/CoroutinesController.kt
+ */
+public class CoroutinesController {
+}
---
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/core/README.md
@@ -0,0 +1,4 @@
+# Core
+
+https://medium.com/hongbeomi-dev/tagged/kotlin
+
