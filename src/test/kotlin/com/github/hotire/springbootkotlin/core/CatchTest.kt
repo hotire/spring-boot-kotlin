b88a93d12c3b4d863712f34d35659a918dd2edf6
hotire <gngh0101@gmail.com>
2021-02-05 00:28:16 +0900

    Verify ifEmpty

--- src/test/kotlin/com/github/hotire/springbootkotlin/core/CatchTest.kt
+++ src/test/kotlin/com/github/hotire/springbootkotlin/core/CatchTest.kt
@@ -1,12 +1,21 @@
 package com.github.hotire.springbootkotlin.core
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CatchTest {
 
     @Test
     fun ifEmpty() {
+        // given
+        val list = listOf<String>()
+        val expected: String = "a"
 
+        // when
+        val result = list.ifEmpty { listOf(expected) }
+
+        // then
+        assertThat(result).contains(expected)
     }
 
     @Test
