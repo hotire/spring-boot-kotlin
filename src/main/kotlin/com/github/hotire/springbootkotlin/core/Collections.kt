09fd56e47341d0ec8cb501f49b60ddbe81a888c7
hotire <gngh0101@gmail.com>
2021-02-05 00:17:15 +0900

    Add CatchTest

--- src/main/kotlin/com/github/hotire/springbootkotlin/core/Collections.kt
+++
@@ -1,8 +0,0 @@
-package com.github.hotire.springbootkotlin.core
-
-
-fun main() {
-    listOf("a", "b")
-            .map { it + 1}
-            .get(1)
-}
---
+++ src/test/kotlin/com/github/hotire/springbootkotlin/core/CatchTest.kt
@@ -0,0 +1,26 @@
+package com.github.hotire.springbootkotlin.core
+
+import org.junit.jupiter.api.Test
+
+class CatchTest {
+
+    @Test
+    fun ifEmpty() {
+
+    }
+
+    @Test
+    fun orEmpty() {
+
+    }
+
+    @Test
+    fun requireNoNulls() {
+
+    }
+
+    @Test
+    fun listOfNotNull() {
+
+    }
+}
