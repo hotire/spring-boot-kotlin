d790a51ba0a46315c0ced4f8b2abf6e46e73e4b2
hotire <gngh0101@gmail.com>
2021-03-19 10:00:47 +0900

    Learn TestFactory DynamicTest

---
+++ src/test/kotlin/com/github/hotire/springbootkotlin/test/junit5/TestFactoryTest.kt
@@ -0,0 +1,20 @@
+package com.github.hotire.springbootkotlin.test.junit5
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+
+internal class TestFactoryTest {
+    @TestFactory
+    fun testSquares() = listOf(
+        1 to 1,
+        2 to 4,
+        3 to 9,
+        4 to 16,
+        5 to 25
+    ).map { (input, expected) ->
+        DynamicTest.dynamicTest("when I calculate $input^2 then I get $expected") {
+            assertThat(input * input).isEqualTo(expected)
+        }
+    }
+}
