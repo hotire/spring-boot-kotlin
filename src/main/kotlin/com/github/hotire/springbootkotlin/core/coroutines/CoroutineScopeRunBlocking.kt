c61d4bf6885c284577e71f89a65dba903a192e57
hotire <gngh0101@gmail.com>
2021-03-30 01:04:23 +0900

    Add ktlintFormat

--- src/main/kotlin/com/github/hotire/springbootkotlin/core/coroutines/CoroutineScopeRunBlocking.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/core/coroutines/CoroutineScopeRunBlocking.kt
@@ -1,10 +1,8 @@
 package com.github.hotire.springbootkotlin.core.coroutines
 
-import com.github.hotire.springbootkotlin.log.LoggerDelegate
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-
 
 suspend fun main() {
 
