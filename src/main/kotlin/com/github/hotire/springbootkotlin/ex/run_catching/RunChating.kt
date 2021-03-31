7a6d8dd7820b316046b3cee1b143d33703ae3185
hotire <gngh0101@gmail.com>
2021-04-01 00:59:54 +0900

    Learn how to use runCatching

--- src/main/kotlin/com/github/hotire/springbootkotlin/ex/run_catching/RunChating.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/ex/run_catching/RunChating.kt
@@ -1,12 +1,15 @@
 package com.github.hotire.springbootkotlin.ex.run_catching
 
 fun main() {
-    runCatching {
-        if (1 == 1) throw RuntimeException()
+    var media = "hello2"
+    val result = runCatching {
         "hello"
     }.onFailure {
         println(it)
     }.onSuccess {
         println(it)
-    }
+        media = it
+    }.getOrNull()
+
+    println(result)
 }
