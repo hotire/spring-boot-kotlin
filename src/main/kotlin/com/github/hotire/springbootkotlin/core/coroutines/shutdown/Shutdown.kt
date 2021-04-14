0fd9682f8aa819f5f825cb426a640fd58582f975
hotire <gngh0101@gmail.com>
2021-04-15 00:29:14 +0900

    Learn shutdown in Coroutine

---
+++ src/main/kotlin/com/github/hotire/springbootkotlin/core/coroutines/shutdown/Shutdown.kt
@@ -0,0 +1,27 @@
+package com.github.hotire.springbootkotlin.core.coroutines.shutdown
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.async
+import java.io.Closeable
+import java.util.concurrent.Executors
+
+fun main() {
+    val threadPool = Executors.newFixedThreadPool(4)
+    threadPool.awaitTermination(10)
+    val dispatcher = threadPool.asCoroutineDispatcher()
+
+    CoroutineScope(dispatcher).async {
+        println("hello : ${Thread.currentThread().name}")
+    }
+    threadPool.shutdown()
+
+    val default = Dispatchers.Default
+    val close = default.asExecutor()
+
+    if (close is Closeable) {
+        close.close()
+    }
+}
