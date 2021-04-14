74badd01f06d50db7259d2d448328cb820219ef0
hotire <gngh0101@gmail.com>
2021-04-15 00:28:27 +0900

    Add comment to coroutine_scheduler

--- src/main/kotlin/com/github/hotire/springbootkotlin/core/coroutines/coroutine_scheduler/CoroutineScheduler.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/core/coroutines/coroutine_scheduler/CoroutineScheduler.kt
@@ -4,6 +4,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 
+/**
+ * @see kotlinx.coroutines.Dispatchers.IO
+ * @see kotlinx.coroutines.Dispatchers.Default
+ * IO / Default 는 동일한 executor == coroutineScheduler 사용하고
+ * 구현체는 CoroutineScheduler를 사용한다.
+ */
 fun main() {
     /**
      * @see kotlinx.coroutines.scheduling.CoroutineScheduler.dispatch()
