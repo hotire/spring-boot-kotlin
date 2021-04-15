c0c7bdde3d4bf1c8bdd003d4cc006240080f777b
hotire <gngh0101@gmail.com>
2021-04-15 23:56:44 +0900

    Apply ktlintFormat

--- src/main/kotlin/com/github/hotire/springbootkotlin/delegate/Delegate.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/delegate/Delegate.kt
@@ -2,11 +2,8 @@ package com.github.hotire.springbootkotlin.delegate
 
 import kotlin.reflect.KProperty
 
-
 class MyDelegate {
     operator fun getValue(thisRef: Any, property: KProperty<*>): String = "hello"
 }
 
-
 class C { val prop: String by MyDelegate() }
-
