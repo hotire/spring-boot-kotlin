65ece2abb9ef79dc4b5ba44b4b63812f0098ec30
hotire <gngh0101@gmail.com>
2021-04-20 01:37:10 +0900

    Fix tailrec

--- src/main/kotlin/com/github/hotire/springbootkotlin/tailrec/Tailrec.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/tailrec/Tailrec.kt
@@ -7,12 +7,14 @@ private fun factorial(n: Long): Long {
 }
 
 // 재귀 함수는 재귀 호출이 함수에 의해 마지막으로 실행되는 경우 꼬리 재귀 입니다.
-private tailrec fun factorialTailrec(n: Long, answer: Long): Long {
-    return if (n == 1L) n
-    else factorialTailrec(n - 1, answer * n)
+private tailrec fun factorialTailrec(n: Long, answer: Long = 1): Long {
+    return if (n == 1L) {
+        println(answer)
+        return n
+    } else factorialTailrec(n - 1, answer * n)
 }
 
 fun main() {
-    factorial(10000)
-    factorialTailrec(10000)
+    println(factorial(10L))
+    println(factorialTailrec(10L))
 }
