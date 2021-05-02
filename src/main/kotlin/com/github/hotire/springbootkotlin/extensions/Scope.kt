54e65edbe135e22fe65cdc973dba1bfac1a09ab4
hotire <gngh0101@gmail.com>
2021-05-02 13:06:48 +0900

    Learn scope of extensions

--- src/main/kotlin/com/github/hotire/springbootkotlin/extensions/Scope.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/extensions/Scope.kt
@@ -5,9 +5,11 @@ class D {
 }
 class C {
     fun baz() { println("C.bar()") }
-    fun D.foo() {   // class C valid
+    fun D.foo() { // class C valid
         bar() // calls D.bar
         baz() // calls C.baz
+        toString() // call D.toString()
+        this@C.toString() // call C.toString()
     }
     fun caller(d: D) {
         d.foo() // call the extension function
